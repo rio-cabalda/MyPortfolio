6a07e77b2abc3e163ff008ce4cfc975578225d59
rio-cabalda <riojoycabalda14@gmail.com>
2023-11-06 00:16:43 +0800

    Update HeroDesktop.jsx

--- src/components/HeroDesktop.jsx
+++ src/components/HeroDesktop.jsx
@@ -2,6 +2,7 @@ import { Facebook, Linkedin, Github } from "lucide-react";
 import useGlobalState from "../store/globalStateStore";
 import avatar from '../assets/hero/avatar.png';
 import { motion } from "framer-motion";
+import CV from '../assets/file/CV.pdf'
 const HeroContent = () => {
     const {theme} = useGlobalState();
 
@@ -29,13 +30,15 @@ return (
                 <motion.p variants={titleVariants} className="text-2xl font-bold tracking-[0.7rem] text-[#0450AB] dark:text-[#fcbf49]  shadow-slate-400 text-shadow-sm">front-end developer</motion.p>
 
             <div  className="flex mt-10 gap-5">
-                {/* <button className="rounded-full px-4 py-2 bg-[#f77f00] dark:bg-[#f77f00] text-black shadow-md shadow-slate-700">Download CV</button> */}
+                <a href={CV} download="Cabalda-CV">
+                    <button className="rounded-full px-4 py-2 bg-[#f77f00] dark:bg-[#f77f00] text-black shadow-md shadow-slate-700">Download CV</button>
+                </a>
                 <motion.div initial="initial" animate="animate" variants={socialVariants} className="flex gap-2">
                     <motion.div variants={socialVariants} className="w-10 h-10 flex justify-center items-center bg-[#28282B] dark:bg-white rounded-full p-2 hover:scale-110 active:scale-90 shadow-md shadow-slate-700 duration-300">
-                        <a href="www.linkedin.com/in/rio-joy-cabalda-5457b2269" rel="noreferrer" target="_blank" > <Linkedin size={25} color={theme === 'light'?'#fff':'#000'} strokeWidth={1}/> </a> 
+                        <a href="https://www.linkedin.com/in/riojoy-cabalda" rel="noreferrer" target="_blank" > <Linkedin size={25} color={theme === 'light'?'#fff':'#000'} strokeWidth={1}/> </a> 
                     </motion.div>
                     <motion.div variants={socialVariants} className="w-10 h-10 flex justify-center items-center bg-[#28282B] dark:bg-white rounded-full p-2 hover:scale-110 active:scale-90 shadow-md shadow-slate-700 duration-300">
-                        <a href="https://github.com/rio-cabalda" rel="noreferrer" target="_blank" > <Github size={25} color={theme === 'light'?'#fff':'#000'} strokeWidth={1}/> </a>
+                        <a href="https://www.github.com/rio-cabalda" rel="noreferrer" target="_blank" > <Github size={25} color={theme === 'light'?'#fff':'#000'} strokeWidth={1}/> </a>
                     </motion.div>
                     <motion.div variants={socialVariants} className="w-10 h-10 flex justify-center items-center bg-[#28282B] dark:bg-white rounded-full hover:scale-110 active:scale-90 shadow-md shadow-slate-700 duration-300">
                         <a href="https://www.facebook.com/rjcabalda" rel="noreferrer" target="_blank" > <Facebook className="-translate-x-[1px]" size={25} color={theme === 'light'?'#fff':'#000000'} strokeWidth={1}/></a>
